deb4e8933ce16ea005f6709e0c5102878af34ae2
Judethedude007 <judethomas2004@gmail.com>
2026-03-14 01:02:46 +0530

    Improve GPU test visibility and shader setup

--- worm_simulator/gpu_test.py
+++ worm_simulator/gpu_test.py
@@ -16,36 +16,42 @@ class WormRenderer(moderngl_window.WindowConfig):
             vertex_shader="""
                 #version 330
                 in vec2 in_pos;
+
                 void main() {
                     gl_Position = vec4(in_pos, 0.0, 1.0);
-                    gl_PointSize = 10.0;
+                    gl_PointSize = 40.0;
                 }
             """,
             fragment_shader="""
                 #version 330
                 out vec4 fragColor;
+
                 void main() {
-                    fragColor = vec4(1.0, 0.4, 0.4, 1.0);
+                    fragColor = vec4(1.0, 0.3, 0.3, 1.0);
                 }
             """,
         )
 
-        # Example worm positions
         points = np.array([
-            [-0.5, 0.0],
-            [-0.2, 0.2],
-            [0.1, -0.1],
-            [0.5, 0.3],
+            [-0.8, -0.8],
+            [-0.8,  0.8],
+            [ 0.8, -0.8],
+            [ 0.8,  0.8],
+            [ 0.0,  0.0],
         ], dtype="f4")
 
         self.vbo = self.ctx.buffer(points.tobytes())
         self.vao = self.ctx.simple_vertex_array(self.program, self.vbo, "in_pos")
 
-    def on_render(self, time, frame_time):
+    def render(self, time, frame_time):
 
         self.ctx.clear(0.1, 0.1, 0.1)
 
         self.vao.render(moderngl.POINTS)
+
+    def on_render(self, time, frame_time):
+        # Keep compatibility with moderngl-window builds that call on_render.
+        self.render(time, frame_time)
 
 
 if __name__ == "__main__":
